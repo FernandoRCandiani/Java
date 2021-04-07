75f457e7aebc0135e4bc92f856248e749925145a
unknown <candianirfernando@gmail.com>
2021-04-06 23:22:52 -0300

    Atividade Spring Security para blog pessoal

---
+++ Java Entregas/blogpessoal 4/blogpessoal/src/main/java/com/generation/blogpessoal/model/Tema.java
@@ -0,0 +1,53 @@
+package com.generation.blogpessoal.model;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name = "tb_tema")
+public class Tema {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotNull
+	private String descricao;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("tema")
+	private Postagem postagem;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Postagem getPostagem() {
+		return postagem;
+	}
+
+	public void setPostagem(Postagem postagem) {
+		this.postagem = postagem;
+	}
+	
+}
